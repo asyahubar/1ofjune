9de00ebbe580dbf5a31f60c874bec56693211d7c
asyahubar <asyagubar@gmail.com>
2022-05-25 23:52:15 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,145 @@
+<script>
+  import mainImg from './assets/ukraine.jpeg'
+  let lang = 'uk'
+  let embOnMap = 'https://www.google.com/maps/place/Embassy+of+Ukraine/@44.7727203,20.4594424,17z/data=!3m1!4b1!4m5!3m4!1s0x475a704efbff6dd5:0xbb490a4f707cb2db!8m2!3d44.7727165!4d20.4616311'
+</script>
+
+<section class="layout">
+  <img src={mainImg} alt="Долоні обіймають сердечко. Авторка: Agnieszka Srokosz" class="main-img"/>
+  <main>
+    <p class="subtitle">За підтримки Посольства України в Сербії та громади</p>
+
+    <h1>Захід до Дня захисту дітей</h1>
+
+    <p>
+      Чекаємо на вас <strong>1 травня о 15:00</strong> у Посольстві України в Сербії за адресою <a href={embOnMap}>вул.Пає Адамова 4 у Белграді</a>.
+    </p>
+
+    <hr>
+
+    <p>Щоб доєднатися, потрібно:</p>
+
+    <ul class="conditions">
+      <li>Попередньо зареєструватися <a href="https://docs.google.com/forms/d/1MoMlMx__uhagZmzeF9nsHnrw5w9i2CmmjUMkiOmZyyw/viewform?edit_requested=true">в Google формі</a></li>
+      <li>Принести з собою малюнок дитини, де зображений мир</li>
+    </ul>
+
+  </main>
+</section>
+
+<style>
+  @font-face {
+    font-family: 'e-Ukraine';
+    src: url('./assets/fonts/e-Ukraine-Thin.ttf') format('ttf');
+    font-weight: 200;
+  }
+  @font-face {
+    font-family: 'e-Ukraine';
+    src: url('./assets/fonts/e-Ukraine-Medium.ttf') format('ttf');
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: 'e-Ukraine';
+    src: url('./assets/fonts/e-Ukraine-Bold.ttf') format('ttf');
+    font-weight: 700;
+  }
+
+  :root {
+    font-family: 'e-Ukraine', 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+  }
+
+  .layout {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
+
+  main {
+    /* text-align: center; */
+    padding: 1em;
+    /* margin: 0 auto; */
+  }
+
+  .main-img {
+    height: 16rem;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (min-width: 800px) {
+    .layout {
+      flex-direction: row;
+    }
+
+    .main-img {
+      height: 100vh;
+      width: 32%;
+    }
+  }
+
+  h1 {
+    color: #0057b8;
+    /* text-transform: uppercase; */
+    font-size: 3.25rem;
+    font-weight: 700;
+    line-height: 1.1;
+    margin: 2rem 0;
+    /* max-width: 14rem; */
+  }
+
+  .subtitle {
+    color: grey;
+    font-size: 1rem;
+    margin-bottom: -1.5rem;
+    font-weight: 400;
+  }
+
+  p {
+    /* max-width: 14rem; */
+    margin: 1rem 0;
+    line-height: 1.35;
+  }
+
+  a {
+    color: #0057b8;
+  }
+
+  hr {
+    border-bottom: 1px dashed #0057b8;
+    border-left: 0;
+    border-top: 0;
+    border-right: 0;
+    margin: 1rem 0;
+  }
+
+  @counter-style emoji {
+    system: cyclic;
+    symbols: "\1F431" "\1F436" "\1F984";
+    suffix: " ";
+  }
+  .conditions {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  .conditions li {
+    margin: 0;
+    padding: .7rem 0;
+    /* background-color: rgba(255, 216, 1, 0.4); */
+    border-radius: 1rem;
+    max-width: 25em;
+  }
+  .conditions li::before {
+    content: '☑';
+    width: 5px;
+    height: 10px;
+    object-fit: contain;
+    margin-inline-end: 1ch;
+  }
+  .conditions li + li {
+    /* margin-top: 1ch; */
+  }
+</style>
